410d21696c07be1da0cf8b062289a6b61cf2979c
DANIELMRJ <jesusdanielmunozrios@gmail.com>
2025-08-18 08:09:26 -0600

    Blackjack en Go primera versión jugable

--- main.go
+++ main.go
@@ -1,86 +1,148 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+var templates = template.Must(template.ParseFiles("templates/index.html"))
 
 type Game struct {
-	Player   []int
-	Dealer   []int
-	Message  string
-	GameOver bool
+	PlayerCards []string `json:"playerCards"`
+	DealerCards []string `json:"dealerCards"`
+	PlayerScore int      `json:"playerScore"`
+	DealerScore int      `json:"dealerScore"`
+	Message     string   `json:"message"`
 }
 
-// cartas posibles (simplificado: As vale 11, J/Q/K = 10)
-var deck = []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11}
+var deck []string
+var player []string
+var dealer []string
 
-var game Game
-
-func drawCard() int {
-	rand.Seed(time.Now().UnixNano())
-	return deck[rand.Intn(len(deck))]
-}
-
-func newGame() {
-	game = Game{
-		Player:   []int{drawCard(), drawCard()},
-		Dealer:   []int{drawCard()},
-		Message:  "Tu turno, elige una acción",
-		GameOver: false,
-	}
-}
-
-func total(hand []int) int {
-	sum := 0
-	for _, c := range hand {
-		sum += c
-	}
-	return sum
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	action := r.URL.Query().Get("action")
-
-	if action == "new" || game.Player == nil {
-		newGame()
-	} else if !game.GameOver {
-		switch action {
-		case "hit":
-			game.Player = append(game.Player, drawCard())
-			if total(game.Player) > 21 {
-				game.Message = "Te pasaste de 21 😢"
-				game.GameOver = true
-			}
-		case "stand":
-			// turno del dealer
-			for total(game.Dealer) < 17 {
-				game.Dealer = append(game.Dealer, drawCard())
-			}
-			// decidir ganador
-			playerScore := total(game.Player)
-			dealerScore := total(game.Dealer)
-
-			if dealerScore > 21 || playerScore > dealerScore {
-				game.Message = "¡Ganaste! 🎉"
-			} else if playerScore == dealerScore {
-				game.Message = "Empate 🤝"
-			} else {
-				game.Message = "Perdiste 😭"
-			}
-			game.GameOver = true
+// Crear y barajar mazo
+func newDeck() []string {
+	suits := []string{"♠", "♥", "♦", "♣"}
+	values := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+	var d []string
+	for _, s := range suits {
+		for _, v := range values {
+			d = append(d, v+s)
 		}
 	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(d), func(i, j int) { d[i], d[j] = d[j], d[i] })
+	return d
+}
 
-	tmpl.Execute(w, game)
+// Calcular puntaje
+func score(cards []string) int {
+	total := 0
+	aces := 0
+	for _, c := range cards {
+		val := c[:len(c)-3] // número/letra (quitando símbolo)
+		switch val {
+		case "A":
+			total += 11
+			aces++
+		case "K", "Q", "J", "10":
+			total += 10
+		default:
+			n, _ := strconv.Atoi(val)
+			total += n
+		}
+	}
+	// Ajustar ases
+	for total > 21 && aces > 0 {
+		total -= 10
+		aces--
+	}
+	return total
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
+
+// Iniciar partida
+func startHandler(w http.ResponseWriter, r *http.Request) {
+	deck = newDeck()
+	player = []string{deck[0], deck[2]}
+	dealer = []string{deck[1], deck[3]}
+	deck = deck[4:]
+
+	game := Game{
+		PlayerCards: player,
+		DealerCards: []string{dealer[0], "??"}, // ocultar una carta dealer
+		PlayerScore: score(player),
+		DealerScore: 0,
+		Message:     "Juego iniciado 🚀",
+	}
+	json.NewEncoder(w).Encode(game)
+}
+
+// Pedir carta
+func hitHandler(w http.ResponseWriter, r *http.Request) {
+	if len(deck) == 0 {
+		deck = newDeck()
+	}
+	player = append(player, deck[0])
+	deck = deck[1:]
+
+	game := Game{
+		PlayerCards: player,
+		DealerCards: []string{dealer[0], "??"},
+		PlayerScore: score(player),
+		Message:     "Pediste carta",
+	}
+
+	if game.PlayerScore > 21 {
+		game.Message = "Te pasaste 💀 ¡Perdiste!"
+		game.DealerCards = dealer
+		game.DealerScore = score(dealer)
+	}
+	json.NewEncoder(w).Encode(game)
+}
+
+// Plantarse
+func standHandler(w http.ResponseWriter, r *http.Request) {
+	for score(dealer) < 17 {
+		dealer = append(dealer, deck[0])
+		deck = deck[1:]
+	}
+
+	playerScore := score(player)
+	dealerScore := score(dealer)
+
+	msg := ""
+	if dealerScore > 21 || playerScore > dealerScore {
+		msg = "¡Ganaste! 🎉"
+	} else if playerScore < dealerScore {
+		msg = "Perdiste 😢"
+	} else {
+		msg = "Empate 🤝"
+	}
+
+	game := Game{
+		PlayerCards: player,
+		DealerCards: dealer,
+		PlayerScore: playerScore,
+		DealerScore: dealerScore,
+		Message:     msg,
+	}
+	json.NewEncoder(w).Encode(game)
 }
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/start", startHandler)
+	http.HandleFunc("/hit", hitHandler)
+	http.HandleFunc("/stand", standHandler)
+
+	log.Println("Servidor en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
